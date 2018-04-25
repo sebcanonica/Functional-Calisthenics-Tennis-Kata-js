b3316bd352f1187bd25726c080b57f75fffcfaa7
Sébastien CANONICA <sebcano.biz@laposte.net>
2018-04-25 10:20:11 +0200

    Initial version

---
+++ app.js
@@ -0,0 +1,97 @@
+const readline = require('readline');
+
+function playGame(game) {
+    if (game.score.endGame) {
+        process.exit();
+    } else {
+        function winPoint(winner) {
+            playGame( {
+                io: game.io,
+                score: game.io.output( decorateDisplay( plusPoint({
+                    ...game.score,
+                    lastPointWinner: winner
+                }) ) ).passthru
+            } );
+        }
+        game.io.inputPromise().then( winPoint );
+    }
+}
+
+function plusPoint(score) {
+    if (score.lastPointWinner == 1) {
+        return { p1: score.p1+1, p2: score.p2 };
+    } else {
+        return { p1: score.p1, p2: score.p2+1 };
+    }
+}
+
+function decorateDisplay(score) {
+    if (score.p1 >= 3 && score.p2 >= 3) {
+        if (score.p1 == score.p2) {
+            return { display: 'Deuce', ...score };
+        } else if (Math.abs(score.p1 - score.p2) == 1) {
+            if (score.p1 > score.p2) {
+                return { display: 'Advantage P1', ...score};
+            } else {
+                return { display: 'Advantage P2', ...score};
+            }
+        } else {
+            if (score.p1 > score.p2) {
+                return { display: 'Winner P1', ...score, endGame: true};
+            } else {
+                return { display: 'Winner P2', ...score, endGame: true};
+            }
+        }
+    } else if ( (score.p1 >= 4 || score.p2 >= 4) && Math.abs(score.p1 - score.p2) >= 2 ) {
+        if (score.p1 > score.p2) {
+            return { display: 'Winner P1', ...score, endGame: true};
+        } else {
+            return { display: 'Winner P2', ...score, endGame: true};
+        }
+    } else {
+        return {
+            display: displayBasePoint(score.p1) + ' - ' + displayBasePoint(score.p2),
+            ...score
+        };
+    }
+}
+
+function displayBasePoint(number) {
+    switch (number) {
+        case 0: return 'Love';
+        case 1: return 'Fifteen';
+        case 2: return 'Thirty';
+        default: return 'Forty';
+    }
+}
+
+function consoleInput(lineReader) {
+    return function() {
+        console.log('creating promise');
+        return new Promise( (resolve, reject) => {
+            lineReader.question('Winner (1 | 2)?', (answer) => {
+                resolve(answer);
+            });
+        }
+    ) };
+}
+
+function consoleOutput(decoratedScore) {
+    return {
+        passthru: decoratedScore,
+        whatever: console.log(decoratedScore.display)
+    };
+}
+
+playGame({
+    score: { p1:0, p2:0 },
+    io: {
+        output : consoleOutput,
+        inputPromise: consoleInput( readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        }) )
+    }
+});
+
+
